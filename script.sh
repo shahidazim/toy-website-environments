5206e1eec5ec4807883f60c25e136b7996fc2923
Shahid Azim <shahid.azim@bjss.com>
2024-06-17 13:18:42 +0100

    updated script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 githubOrganizationName='shahidazim'
 githubRepositoryName='toy-website-environments'
 
-applicationRegistrationDetails=$(az ad app create --display-name 'toy-website-environments')
+applicationRegistrationDetails=$(az ad app create --display-name $githubRepositoryName)
 applicationRegistrationObjectId=$(echo $applicationRegistrationDetails | jq -r '.id')
 applicationRegistrationAppId=$(echo $applicationRegistrationDetails | jq -r '.appId')
 
@@ -17,12 +17,12 @@ az role assignment create --role Contributor \
 
 az ad app federated-credential create \
    --id $applicationRegistrationObjectId \
-   --parameters "{\"name\":\"toy-website-environments_main\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:ref:refs/heads/main\",\"description\":\"The federated identity credentials for main branch\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+   --parameters "{\"name\":\"${githubRepositoryName}_main\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:ref:refs/heads/main\",\"description\":\"The federated identity credentials for main branch\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
 
 az ad app federated-credential create \
    --id $applicationRegistrationObjectId \
-   --parameters "{\"name\":\"toy-website-environments_pull-request\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:pull_request\",\"description\":\"The federated identity credentials for pull request\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+   --parameters "{\"name\":\"${githubRepositoryName}_pull-request\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:pull_request\",\"description\":\"The federated identity credentials for pull request\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
 
 az ad app federated-credential create \
    --id $applicationRegistrationObjectId \
-   --parameters "{\"name\":\"toy-website-environments_env-production\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:environment:Production\",\"description\":\"The federated identity credentials for Production environment\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
+   --parameters "{\"name\":\"${githubRepositoryName}_env-production\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:${githubOrganizationName}/${githubRepositoryName}:environment:Production\",\"description\":\"The federated identity credentials for Production environment\",\"audiences\":[\"api://AzureADTokenExchange\"]}"
